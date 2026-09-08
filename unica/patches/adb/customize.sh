f027a18fe703c92f2f7dfc60dd1ee8d70adeae35
Miguel Barreto <miguel03barreto@gmail.com>
2026-09-08 17:48:04 -0300

    (feat)adb: force adb by default

--- unica/patches/adb/customize.sh
+++ unica/patches/adb/customize.sh
@@ -1,9 +1,3 @@
-# Only enable on debug builds
-if ! $DEBUG; then
-    LOG "\033[0;33m! Non-debug build detected. Skipping\033[0m"
-    return 0
-fi
-
 # Start adbd on boot
 # https://android.googlesource.com/platform/packages/modules/adb/+/refs/heads/main/docs/dev/how_adbd_starts.md
 SET_PROP_IF_DIFF "product" "persist.sys.usb.config" "$(GET_PROP "product" "persist.sys.usb.config"),adb"
