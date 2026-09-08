2825db436d8f7a7a95d45e1825dadd9224a8fc72
miguelbarretoo <miguel03barreto@gmail.com>
2026-09-08 20:33:18 -0300

    test pt2

---
+++ target/y2slte/patches/kernel/customize.sh
@@ -0,0 +1,28 @@
+KERNEL_ZIP="$MODPATH/kernel.zip"
+LOG "- Extracting Artisan kernel"
+
+if [ -f "$WORK_DIR/kernel/boot.img" ]; then
+    EVAL "rm -f \"$WORK_DIR/kernel/boot.img\""
+fi
+if [ -f "$WORK_DIR/kernel/dtbo.img" ]; then
+    EVAL "rm -f \"$WORK_DIR/kernel/dtbo.img\""
+fi
+if [ -f "$WORK_DIR/kernel/dtbo_lte.img" ]; then
+    EVAL "rm -f \"$WORK_DIR/kernel/dtbo_lte.img\""
+fi
+
+KERNEL_TMP_DIR="$WORK_DIR/kernel/tmp_extract"
+EVAL "rm -rf \"$KERNEL_TMP_DIR\""
+EVAL "mkdir -p \"$KERNEL_TMP_DIR\""
+
+EVAL "unzip -o \"$KERNEL_ZIP\" -d \"$KERNEL_TMP_DIR\""
+
+LOG "- Injecting kernel binaries"
+
+EVAL "find \"$KERNEL_TMP_DIR\" -iname 'boot.img' -exec cp {} \"$WORK_DIR/kernel/boot.img\" \;"
+EVAL "find \"$KERNEL_TMP_DIR\" -iname 'dtbo.img' -exec cp {} \"$WORK_DIR/kernel/dtbo.img\" \;"
+EVAL "find \"$KERNEL_TMP_DIR\" -iname 'dtbo_lte.img' -exec cp {} \"$WORK_DIR/kernel/dtbo_lte.img\" \;"
+
+EVAL "rm -rf \"$KERNEL_TMP_DIR\""
+
+unset KERNEL_ZIP KERNEL_TMP_DIR
