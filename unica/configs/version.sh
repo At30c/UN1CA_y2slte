3c27207fe9722b7cf8d6b850b0ab49a4175e2150
Miguel Barreto <miguel03barreto@gmail.com>
2026-09-16 14:14:40 -0300

    Update version number and codename in version.sh

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Only the below variable(s) need to be changed!
-VERSION_MAJOR=4
+VERSION_MAJOR=5
 VERSION_MINOR=0
 VERSION_PATCH=0
 
@@ -10,4 +10,4 @@ VERSION_PATCH=0
 #
 # Version name
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
-ROM_CODENAME="Aether"
+ROM_CODENAME="Akita"
